c4d640d22acd347832958cef91dfbb18546b98c1
Gabe Jackson <gj@mail.co.de>
2017-09-21 15:20:51 -0400

    Merge in work from @JeffKatzy in learn-co-curriculum/js-data-structures-object-lab and fix errors

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
-# JavaScript Objects Lab
+# Objects Lab
 
 ## Overview
-
-In this lab, we'll create objects and perform various operations on them. 
+In this lab, we'll create `driver` objects and perform various operations on them.
 
 ## Instructions
 
 Be sure to run the tests to get a feel for the types of problems this lab is
-asking you to solve. In particular, you'll need to define a `recipes` object and
+asking you to solve. In particular, you'll need to define a `driver` object and
 then apply certain updates (destructively and non-destructively) in various
 functions.
 
-![recipes](http://i.giphy.com/l0HFjaGmrbHanFXNe.gif)
+You'll be writing four functions:
+
+ * `updateDriverWithKeyAndValue`- this function should take in a `driver` object, a `key` and a `value`. The function should not mutate the `driver` parameter and return a new `driver` that has an updated `value` for the `key` passed int
+ * `destructivelyUpdateDriverWithKeyAndValue` - this function should work the same as `updateDriverWithKeyAndValue` but it *should* mutate the `driver` parameter passed in.
+ * `deleteFromDriverByKey` - this function should take in a `driver` object and a `key`. It should delete the `key`/`value` pair for the `key` that was passed in from the `driver` object. This should all not actually `mutate` the `driver` passed in
+ * `destructivelyDeleteFromDriverByKey` - this function should work the same as `deleteFromDriverByKey` but it *should* mutate the `driver` passed in.
 
 Good luck!
 
-**HINT**: You might find `deleteFromObjectByKey` to be a bit hard to write non-destructively. Think about how we learned to use `Object.assign`. What happens if we do
+**HINT**: You might find `deleteFromDriverByKey` to be a bit hard to write non-destructively. Think about how we learned to use `Object.assign`. What happens if we do
 
-``` javascript
+```js
 var obj = { foo: 'bar' }
 
 var newObj = Object.assign({}, obj)
@@ -37,4 +41,4 @@ Hmmmmm...
 
 - [MDN: Object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object)
 
-<p class='util--hide'>View <a href='https://learn.co/lessons/javascript-objects-lab'>Javascript Objects Lab</a> on Learn.co and start learning to code for free.</p>
+<p class='util--hide'>View <a href='https://learn.co/lessons/js-data-structures-objects-lab'>Objects Lab</a> on Learn.co and start learning to code for free.</p>
---
+++ test/indexTest.js
@@ -0,0 +1,77 @@
+const expect = chai.expect;
+
+/*global describe, it */
+
+describe('drivers', function() {
+
+  it('defines a `driver` driver', function() {
+    expect(typeof driver).to.equal('object')
+  })
+
+  describe('updatedriverWithKeyAndValue(driver, key, value)', function() {
+    it('returns an driver with the orignal key value pairs and the new key value pair', function() {
+      let driver = { name: 'sam' }
+
+      expect(updateDriverWithKeyAndValue(driver, 'address', '11 Broadway')).to.eql({
+        name: 'sam',
+        address: '11 Broadway'
+      })
+    })
+
+    it('it does not modify the original driver, but rather returns a clone with the new data', function() {
+      let driver = { name: 'sam' }
+
+      updateDriverWithKeyAndValue(driver, 'address', '11 Broadway')
+
+      expect(driver['address']).to.equal(undefined)
+    })
+  })
+
+  describe('destructivelyUpdatedriverWithKeyAndValue(driver, key, value)', function() {
+    it('updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver', function() {
+      let driver = { name: 'sam' }
+
+      expect(destructivelyUpdateDriverWithKeyAndValue(driver, 'address', '12 Broadway')).to.eql({
+        name: 'sam',
+        address: '12 Broadway'
+      })
+
+      expect(driver).to.eql({
+        name: 'sam',
+        address: '12 Broadway'
+      })
+    })
+  })
+
+  describe('deleteFromDriverByKey(driver, key)', function() {
+    it('deletes `key` from a clone of driver and returns the new driver (it is non-destructive)', function() {
+      let driver = { name: 'sam' }
+      let newdriver = deleteFromDriverByKey(driver, 'name')
+
+      expect(newdriver['name']).to.equal(undefined)
+    })
+
+    it('does not modify the original driver (it is non-destructive)', function() {
+      let driver = { name: 'sam' }
+
+      deleteFromDriverByKey(driver, 'name')
+      expect(driver['name']).to.equal('sam')
+    })
+  })
+
+  describe('destructivelyDeleteFromdriverByKey(driver, key)', function() {
+    it('returns driver without the delete key/value pair', function() {
+      let driver = { name: 'sam' }
+
+      let newdriver = destructivelyDeleteFromDriverByKey(driver, 'name');
+      expect(newdriver['name']).to.equal(undefined)
+    })
+
+    it('modifies the original driver', function() {
+      let driver = { name: 'sam' }
+      let newdriver = destructivelyDeleteFromDriverByKey(driver, 'name');
+
+      expect(driver['name']).to.equal(undefined)
+    })
+  })
+})
