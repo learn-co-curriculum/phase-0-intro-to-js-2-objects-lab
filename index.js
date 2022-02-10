5158d0b171457bfe036159601a41a530c143964e
Gracie <m.graciemcguire@gmail.com>
2022-02-10 16:25:39 -0500

    fixes solution

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const employee = {
+const driver = {
   name: "Max",
-  streetAddress: "5 Main Street"
+  streetAddress: "5 Main Street",
 };
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
+function updateDriverWithKeyAndValue(driver, key, value) {
   // Alternate using ES6 Spread operators:
-  // return { ...employee, ...{ [key]: value } }
-  return Object.assign({}, employee, { [key]: value });
+  // return { ...driver, ...{ [key]: value } }
+  return Object.assign({}, driver, { [key]: value });
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-  employee[key] = value;
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
 
-  return employee;
+  return driver;
 }
 
-function deleteFromEmployeeByKey(employee, key) {
+function deleteFromDriverByKey(driver, key) {
   // Alternate using ES6 Spread operators:
-  // const newObj = { ...employee }
-  const newObj = Object.assign({}, employee);
+  // const newObj = { ...driver }
+  const newObj = Object.assign({}, driver);
 
   delete newObj[key];
 
   return newObj;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-  delete employee[key];
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
 
-  return employee;
+  return driver;
 }
