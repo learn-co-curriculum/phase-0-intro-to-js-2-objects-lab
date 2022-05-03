0606684c8972ac5482d85de3c21abfcc13c3eee8
Eva Reitzel <evareitzel@gmail.com>
2022-05-02 22:25:22 -0400

    commit tree 4d217d06206bdd4f14e2d76fafc5eb2f315ff8b6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+const employee = {
+    name:          'Keith Richards',
+    streetAddress: '1 Main St',
+}
+
+// 1
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+
+// 2
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+    }
+
+// 3
+function deleteFromEmployeeByKey(employee, key) {
+    const copy = { ...employee };
+    delete copy[key];
+    return copy;
+}
+
+// 4
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+     delete employee[key];
+     return employee;
+}
