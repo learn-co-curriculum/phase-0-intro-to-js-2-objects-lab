203f2b1af552700c25d5b97a191dd987dcb207a0
katya2108 <katerinavodeiko@gmail.com>
2022-07-08 23:05:16 -0600

    commit tree fc0b6b8d9a3408ee4006b4e0a533855cec70ec76

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+name:'value1',
+streetAddress:'value2',
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+return {...obj,
+  [key]:value,
+};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key]=value;
+  return obj
+  } 
+
+function deleteFromEmployeeByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+   delete newObj[key];
+   return newObj;
+    } 
+
+    function destructivelyDeleteFromEmployeeByKey(obj, key) {
+      delete obj[key];
+      return obj;
+    }
