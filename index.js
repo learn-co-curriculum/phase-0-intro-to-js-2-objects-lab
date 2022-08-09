1114756189688767ae389b710e18d59ff8f3c88a
peter <+>
2022-08-08 22:41:46 -0400

    commit tree 389e0abe8b1a32a3da0437761ece7a3829c0102d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {};
+employee["name"] = "";
+employee["streetAddress"] = "";
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+
+    return newEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+  }
+
+function deleteFromEmployeeByKey (employee, key){
+    const newEmployee = {...employee};
+    
+    delete newEmployee[key];
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key){  
+    delete employee[key];
+
+    return employee;
+}
+
