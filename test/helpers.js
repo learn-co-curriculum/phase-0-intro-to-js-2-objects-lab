dced36d14243bcccf21657d310c22d0966dced42
yousifr <yousifr.ismail@gmail.com>
2023-01-23 21:33:03 +0000

    completed assignment

--- test/helpers.js
+++ test/helpers.js
@@ -1,21 +1,27 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+const chai = require("chai");
+global.expect = chai.expect;
+const fs = require("file-system");
+const jsdom = require("mocha-jsdom");
+const path = require("path");
 const babel = require("@babel/core");
-const url = "http://localhost"
+const url = "http://localhost";
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+const html = fs.readFileSync(
+  path.resolve(__dirname, "..", "index.html"),
+  "utf-8"
+);
 
 const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['@babel/env']
+  path.resolve(__dirname, "..", "index.js"),
+  {
+    presets: ["@babel/env"],
   }
 );
 
-const src = babelResult.code
+const src = babelResult.code;
 
 jsdom({
-  html, src, url
+  html,
+  src,
+  url,
 });
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,70 +1,76 @@
-require ( './helpers.js' );
-
-describe('employees', function() {
-  describe('updateEmployeeWithKeyAndValue(employee, key, value)', function () {
+describe("drivers", function () {
+  describe("updateDriverWithKeyAndValue(driver, key, value)", function () {
     beforeEach(function () {
-      for (const key in employee) {
-        delete employee[key];
+      for (const key in driver) {
+        delete driver[key];
       }
 
-      employee.name = 'Sam';
+      driver.name = "Sam";
     });
 
-    it('returns an employee with the original key value pairs and the new key value pair', function () {
-      expect(updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway')).to.eql({
-        name: 'Sam',
-        streetAddress: '11 Broadway'
+    it("returns a driver with the original key value pairs and the new key value pair", function () {
+      expect(
+        updateDriverWithKeyAndValue(driver, "address", "11 Broadway")
+      ).to.eql({
+        name: "Sam",
+        address: "11 Broadway",
       });
     });
 
-    it('it does not modify the original employee, but rather returns a clone with the new data', function () {
-      updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+    it("it does not modify the original driver, but rather returns a clone with the new data", function () {
+      updateDriverWithKeyAndValue(driver, "address", "11 Broadway");
 
-      expect(employee['streetAddress']).to.equal(undefined);
+      expect(driver["address"]).to.equal(undefined);
     });
   });
 
-  describe('destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)', function () {
-    it('updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee', function () {
-      expect(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')).to.eql({
-        name: 'Sam',
-        streetAddress: '12 Broadway'
+  describe("destructivelyUpdateDriverWithKeyAndValue(driver, key, value)", function () {
+    it("updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver", function () {
+      expect(
+        destructivelyUpdateDriverWithKeyAndValue(
+          driver,
+          "address",
+          "12 Broadway"
+        )
+      ).to.eql({
+        name: "Sam",
+        address: "12 Broadway",
       });
 
-      expect(employee).to.eql({
-        name: 'Sam',
-        streetAddress: '12 Broadway'
+      expect(driver).to.eql({
+        name: "Sam",
+        address: "12 Broadway",
       });
     });
   });
 
-  describe('deleteFromEmployeeByKey(employee, key)', function () {
-    it('deletes `key` from a clone of employee and returns the new employee (it is non-destructive)', function () {
-      let newEmployee = deleteFromEmployeeByKey(employee, 'name');
+  describe("deleteFromDriverByKey(driver, key)", function () {
+    it("deletes `key` from a clone of driver and returns the new driver (it is non-destructive)", function () {
+      let newdriver = deleteFromDriverByKey(driver, "name");
 
-      expect(newEmployee['name']).to.equal(undefined);
-      expect(typeof newEmployee).to.equal('object');
+      expect(newdriver["name"]).to.equal(undefined);
+      expect(typeof newdriver).to.equal("object");
     });
 
-    it('does not modify the original employee (it is non-destructive)', function () {
-      deleteFromEmployeeByKey(employee, 'name');
+    it("does not modify the original driver (it is non-destructive)", function () {
+      deleteFromDriverByKey(driver, "name");
 
-      expect(employee['name']).to.equal('Sam');
+      expect(driver["name"]).to.equal("Sam");
     });
   });
 
-  describe('destructivelyDeleteFromEmployeeByKey(employee, key)', function () {
-    it('returns employee without the deleted key/value pair', function () {
-      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+  describe("destructivelyDeleteFromDriverByKey(driver, key)", function () {
+    it("returns driver without the delete key/value pair", function () {
+      let newdriver = destructivelyDeleteFromDriverByKey(driver, "name");
 
-      expect(newEmployee['name']).to.equal(undefined);
+      expect(newdriver["name"]).to.equal(undefined);
     });
 
-    it('modifies the original employee', function () {
-      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+    it("modifies the original driver", function () {
+      let newdriver = destructivelyDeleteFromDriverByKey(driver, "name");
 
-      expect(employee['name']).to.equal(undefined);
-      expect(employee).to.equal(newEmployee);
+      expect(driver["name"]).to.equal(undefined);
+      expect(driver).to.equal(newdriver);
     });
   });
 });
