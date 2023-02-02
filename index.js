10f5e05d13fe5a6a23e5b99629e521cf5966296d
VeraKulemina <verakulemina@gmail.com>
2023-02-02 12:31:37 -0700

    commit tree 38c545071c5f1d7b5e103ed183967b681cf7dfbc

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const employee = {
+  name: "Max",
+  streetAddress: "5 Main Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, ...{ [key]: value } }
+}
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-  
-    return employee;
-  }
+  employee[key] = value;
+
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newObj = { ...employee }
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
