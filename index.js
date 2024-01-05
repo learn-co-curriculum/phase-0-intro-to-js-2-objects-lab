280ef5cd3db30fdecf8df362f5633418729edf35
Pedro Alarcon <pedroa54@icloud.com>
2024-01-05 11:34:15 -0600

    adds phase-0-intro-to-js-2-objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+let employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = {};
+
+  for (const prop in employee) {
+    updatedEmployee[prop] = employee[prop];
+  }
+
+  updatedEmployee[key] = value;
+
+  return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const updatedEmployee = {};
+
+  for (const prop in employee) {
+    if (prop !== key) {
+      updatedEmployee[prop] = employee[prop];
+    }
+  }
+
+  return updatedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
